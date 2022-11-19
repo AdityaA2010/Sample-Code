7636661fe8dcd54c4eeb68e45cf7728f8d16e0a9
AdityaA2010 <118632582+AdityaA2010@users.noreply.github.com>
2022-11-19 13:39:10 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var ball;
+
+function setup() {
+  createCanvas(400,400);
+  ball=createSprite(350,200,20,20);
+  ball.shapeColor="white";
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites()
+}
+
+
+
+
